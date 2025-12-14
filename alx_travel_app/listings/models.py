d7ba21770be4dee09b683e25b4b5e3cef14245cb
Kamohelo <kamohelotshabalala@proton.me>
2025-12-14 17:51:32 +0200

    Add code in listings/models/

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Listing(models.Model):
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Booking(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    check_in = models.DateField()
+    check_out = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.listing}"
+
+
+class Review(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(rating__gte=1, rating__lte=5),
+                name="rating_between_1_and_5"
+            )
+        ]
+
+    def __str__(self):
+        return f"Review by {self.user} for {self.listing}"
